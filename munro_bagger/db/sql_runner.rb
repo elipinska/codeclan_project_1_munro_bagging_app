0bfcc359679678f635bb44425722ab31c4c16310
elipinska <ewa.lipinska@hotmail.com>
2018-04-07 00:04:54 +0100

    Prepare for deployment

--- munro_bagger/db/sql_runner.rb
+++ munro_bagger/db/sql_runner.rb
@@ -4,7 +4,8 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'munro_bagger', host: 'localhost' })
+      db_info = ENV['DATABASE_URL'].nil? ? {dbname: 'munro_bagger', host: 'localhost'} : ENV['DATABASE_URL']
+      db = PG.connect(db_info)
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
